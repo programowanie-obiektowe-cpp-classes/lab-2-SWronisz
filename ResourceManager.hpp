86c87cc3d00fdd9d2e96b362f40c8808c2f41a07
Sebastian Wronisz <134628396+SWronisz@users.noreply.github.com>
2023-11-04 15:46:19 +0100

    Update ResourceManager.hpp

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,132 +4,39 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
-};
-
-#include <iostream>
-using namespace std;
-
-class Informer {
-public:
-    Informer() {
-        //std::cout << "Konstruktor" << '\n';
+    public:
+    ResourceManager() {
+        resource = new Resource();
     }
-    ~Informer() {
-        //std::cout << "Destruktor" << '\n';
+    ResourceManager(const ResourceManager& other) {
+        resource = new Resource(*other.resource);
     }
-};
-
-class Wektor2D {
-public:
-    static int num_wek;
-    Informer inf;
-    Wektor2D() {};
-    Wektor2D(double wspx, double wspy) {
-        x = wspx;
-        y = wspy;
-        num_wek++;
+    double get() {
+        return resource->get();
     }
-    ~Wektor2D() { num_wek--; }
-    void setX(double X) { x = X; }
-    void setY(double Y) { y = Y; }
-    int getX() { return x; }
-    int getY() { return y; }
-    void norm() {
-        double  n = sqrt(x * x + y * y);
-        cout << n << '\n';
+    ResourceManager(ResourceManager&& other) {
+        resource = other.resource;
+        other.resource = nullptr;
     }
-    void print() {
-        cout << "x = " << x << '\n';
-        cout << "y = " << y << '\n';
+    ResourceManager& operator=(const ResourceManager& other) {
+        delete resource;
+        resource = new Resource(*other.resource);
+        return *this;
     }
-    Wektor2D operator+(const Wektor2D& other) {
-        return Wektor2D(x + other.x, y + other.y);
+    ResourceManager& operator=(ResourceManager&& other) noexcept {
+        delete resource;
+        resource = other.resource;
+        other.resource = nullptr;
+        return *this;
     }
-    Wektor2D operator*(const Wektor2D& other) {
-        return Wektor2D(x * other.x, y * other.y);
-    }
-    friend ostream& operator<<(ostream& os, const Wektor2D& other);
-    static int populacja() {
-        return num_wek;
-    }
-
-private:
-    
-    double x;
-    double y;
-};
-
-int Wektor2D::num_wek = 0;
-
-ostream& operator<<(ostream& os, const Wektor2D& other)
-{
-    os << other.x << ", " << other.y;
-    return os;
-}
-
-class Wektor {
-public:
-    Wektor(int n) : dlugosc{ n } {
-        pojemnosc = 20;
-        tablica = new double[pojemnosc];
-        for (int i = 0; i < n; i++) {
-            tablica[i] = 0;
-        }
-    }
-    ~Wektor() {
-        delete[] tablica;
-    }
-    int getdlugosc() {
-        return dlugosc;
-    };
-    int getpojemnosc() {
-        return pojemnosc;
-    }
-    void print() {
-        //printf("%d", dlugosc);
-        for (int i = 0; i < dlugosc; i++)
+    ~ResourceManager() {
+        if (resource != nullptr)
         {
-            printf("%f \t", tablica[i]);
+            delete resource;
         }
     }
-    void zmienDlugosc(int n) {
-        if (n <= pojemnosc && n <= dlugosc) {
-            dlugosc = n;
-        }
-        else if (n <= pojemnosc && n > dlugosc) {
-            dlugosc = n;
-            for (int i = 0; i < dlugosc; i++)
-            {
-                tablica[i] = 1;
-            }
-        }
-        else if (n > pojemnosc) {
-            double* nowatablica = new double[n];
-            for (int i = 0; i < dlugosc; i++)
-            {
-                nowatablica[i] = tablica[i];
-            }
-            for (int i = dlugosc; i < n; i++)
-            {
-                nowatablica[i] = 2.;
-            }
-            delete[] tablica;
-            tablica = nowatablica;
-            dlugosc = n;
-            pojemnosc = 50;
-        }
-    }
-    double& operator[](int indeks) {
-        if (indeks < dlugosc) {
-            return tablica[indeks];
-        }
-        else {
-            zmienDlugosc(indeks);
-        }
-    };
+
 private:
-    double* tablica;
-    int dlugosc;
-    int pojemnosc;
+    Resource* resource;
 };
+
